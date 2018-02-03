c92c05f9883ff04fcfb78e5c6d65d24802422d82
XiaoxiaLin <xiaoxia908@gmail.com>
2018-02-03 15:21:57 +0100

    day5

--- 5/loops.clj
+++
@@ -1,7 +0,0 @@
-(use '[clojure.string :only (split triml)])
-
-(let [N_t (read-line) 
-      N (Integer/parseInt N_t)]
-  (doseq [i (range 1 11)]
-    (println (str N " x " i " = " (* N i)))))
-
--- 5/loops.py
+++
@@ -1,6 +0,0 @@
-#!python
-
-N = int(input().strip())
-
-for i in range(1, 11):
-    print("{} x {} = {}".format(N, i, N * i))
