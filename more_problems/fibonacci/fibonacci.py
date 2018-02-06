3a85013a28b0271282377a2aa7f133f824670a98
XiaoxiaLin <xiaoxia908@gmail.com>
2018-02-06 20:16:53 +0100

    fibonacci

--- more_problems/fibonacci/fibonacci.py
+++ more_problems/fibonacci/fibonacci.py
@@ -14,3 +14,17 @@ def fibonacci(n):
     
 n = int(input())
 print(fibonacci(n))
+
+
+"""
+# more fancy way:
+ 
+def mem_fib(n, _cache={}):
+    '''efficiently memoized recursive function, returns a Fibonacci number'''
+    if n in _cache:
+        return _cache[n]
+    elif n > 1:
+        return _cache.setdefault(n, mem_fib(n-1) + mem_fib(n-2))
+    return n
+
+"""
