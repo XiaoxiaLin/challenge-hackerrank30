95e910439de911db40c15caffa6fed0375603a09
Xiaoxia Lin <XiaoxiaLin@users.noreply.github.com>
2018-02-06 19:26:13 +0100

    day8

--- day_8/dictionaries_and_maps.py
+++ day_8/dictionaries_and_maps.py
@@ -1,26 +1,5 @@
 #!/bin/python3
 
-n = int(input().strip())
-phonebook = {}
-for i in range(n):
-    line = input()
-    k, v = line.split()
-    phonebook[k] = v
-
-while True:
-    try:
-        number = input()
-    except EOFError:
-        break
-    if number in phonebook:
-        print("{}={}".format(number, phonebook[number]))
-    else:
-        print("Not found")
-
-
-"""
-# my version:
-
 d = {}
 for i in range(int(input())):
     k, v = input().split()
@@ -29,13 +8,15 @@ while True:
     try:
         name = input()
         if name in d:
-            print ('%s=%s' % (name, d[name]))
+            #print ('%s=%s' % (name, d[name]))
+            print("{}={}".format(name, d[name]))
         else:
             print('Not found')
-    except:
+    except EOFError:
         break
 
 
+"""
 # shorter version:
 
 n = int(input())
